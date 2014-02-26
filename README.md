8d5c1d3e85a633c4fa8bd72f4917265624dc6632
nverba <distantstate@gmail.com>
2014-02-26 13:20:46 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### A bare-bones implementation of a draggable lists directive for AngularJS.
+
+This is currently in early development and has yet to be tested in all browsers.
+I intend to develop a number of pluggable themes that employ css animations and effects to make things a bit nicer.
---
+++ draggable-list.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('draggableList', [])
+  .directive('draggableList', function () {
+    var dragData = {};
+    return {
+      restrict: 'A',
+      scope: {
+        draggableList: '=',
+      },
+      link: function (scope, elem, attrs) {
+
+        elem.bind('dragstart', function (e) {
+          e.dataTransfer.setData('from_index', scope.$parent.$index);
+          dragData.elem = e.srcElement.parentNode;
+
+        });
+
+        elem.bind('dragenter', function (e) {
+        });
+
+        elem.bind('dragover', function (e) {
+          // prevent draggable from interacting with other draggable lists
+          if (e.srcElement.parentNode === dragData.elem) e.preventDefault();
+        });
+
+        elem.bind('dragleave', function (e) {
+        });
+
+        elem.bind('drop', function (e) {
+          e.preventDefault();
+          scope.$apply(function () {
+            scope.draggableList.splice(scope.$parent.$index, 0, scope.draggableList.splice(e.dataTransfer.getData('from_index'), 1)[0]);
+          });
+        });
+
+        elem.bind('dragend', function (e) {
+          dragData = {};
+        });
+      }
+    };
+  });
