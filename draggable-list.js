865c25f00a93ecdc0bb533b5de73b6804a9c11a1
nverba <distantstate@gmail.com>
2014-02-27 10:57:28 +0000

    complete base directive working

--- draggable-list.js
+++ draggable-list.js
@@ -12,19 +12,28 @@ angular.module('draggableList', [])
 
         elem.bind('dragstart', function (e) {
           e.dataTransfer.setData('from_index', scope.$parent.$index);
-          dragData.elem = e.srcElement.parentNode;
-
+          dragData.elem = e.target || e.srcElement;
+          // clone the original array - used to reset scope later
+          dragData.origin = angular.copy(scope.draggableList);
         });
 
         elem.bind('dragenter', function (e) {
+          if ((e.target || e.srcElement) !== dragData.elem) { // reduces flickering
+
+            scope.$apply(function () {
+              scope.draggableList.splice(scope.$parent.$index, 0, scope.draggableList.splice(e.dataTransfer.getData('from_index'), 1)[0]);
+            });
+          }
         });
 
         elem.bind('dragover', function (e) {
-          // prevent draggable from interacting with other draggable lists
-          if (e.srcElement.parentNode === dragData.elem) e.preventDefault();
+          // only prevent default on siblings - stops interaction between separate lists
+          if ((e.target.parentNode || e.srcElement.parentNode) === dragData.elem.parentNode) e.preventDefault();
         });
 
         elem.bind('dragleave', function (e) {
+          // reset scope to origin after every leave event
+          angular.copy(dragData.origin, scope.draggableList);
         });
 
         elem.bind('drop', function (e) {
