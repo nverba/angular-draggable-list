a7f222ada997d27b970ed044ca5b38da94b6cfe5
nverba <distantstate@gmail.com>
2014-02-27 10:52:29 +0000

    draggable list working - updates scope on drag-over drag-leave

--- js/draggable-list.js
+++ js/draggable-list.js
@@ -12,18 +12,13 @@ angular.module('draggableList', [])
 
         elem.bind('dragstart', function (e) {
           e.dataTransfer.setData('from_index', scope.$parent.$index);
-          dragData.elem = e.target.parentNode || e.srcElement.parentNode;
-
+          dragData.elem = e.target || e.srcElement;
+          // clone the original array - used to reset scope later
+          dragData.origin = angular.copy(scope.draggableList);
         });
 
         elem.bind('dragenter', function (e) {
-        });
-
-        elem.bind('dragover', function (e) {
-          // prevent draggable from interacting with other draggable lists
-          if ((e.target.parentNode || e.srcElement.parentNode) === dragData.elem) {
-
-            e.preventDefault();
+          if ((e.target || e.srcElement) !== dragData.elem) { // reduces flickering
 
             scope.$apply(function () {
               scope.draggableList.splice(scope.$parent.$index, 0, scope.draggableList.splice(e.dataTransfer.getData('from_index'), 1)[0]);
@@ -31,7 +26,14 @@ angular.module('draggableList', [])
           }
         });
 
+        elem.bind('dragover', function (e) {
+          // only prevent default on siblings - stops interaction between separate lists
+          if ((e.target.parentNode || e.srcElement.parentNode) === dragData.elem.parentNode) e.preventDefault();
+        });
+
         elem.bind('dragleave', function (e) {
+          // reset scope to origin after every leave event
+          angular.copy(dragData.origin, scope.draggableList);
         });
 
         elem.bind('drop', function (e) {
