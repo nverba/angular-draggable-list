172ae3c695c6a0d021f4e8c8208780b925d84fe6
nverba <distantstate@gmail.com>
2014-03-02 12:11:37 +0000

    stability update - supports multiple lists on page

--- js/draggable-list.js
+++ js/draggable-list.js
@@ -14,35 +14,38 @@ angular.module('draggableList', [])
           e.dataTransfer.setData('text/plain', 'Firefox wont drag without this???');
           dragData.from_index = scope.$parent.$index;
           dragData.elem = e.target || e.srcElement;
-          // clone the original array - used to reset scope later
-          dragData.origin = angular.copy(scope.draggableList);
+          dragData.origin = angular.copy(scope.draggableList); // clone the original array - used to reset scope later
         });
 
         elem.bind('dragenter', function (e) {
-          if ((e.target || e.srcElement) !== dragData.elem) { // reduces flickering
+          if ((e.target || e.srcElement).parentElement === dragData.elem.parentElement) { // contain events to list origin
+            if ((e.target || e.srcElement) !== dragData.elem) { // reduces flickering
+              e.preventDefault();
 
-            scope.$apply(function () {
-              scope.draggableList.splice(scope.$parent.$index, 0, scope.draggableList.splice(dragData.from_index, 1)[0]);
-            });
+              scope.$apply(function () {
+                scope.draggableList.splice(scope.$parent.$index, 0, scope.draggableList.splice(dragData.from_index, 1)[0]);
+              });
+            }
           }
         });
 
         elem.bind('dragover', function (e) {
-          // only prevent default on siblings - stops interaction between separate lists
           e.preventDefault();
         });
 
         elem.bind('dragleave', function (e) {
-          // reset scope to origin after every leave event
-          angular.copy(dragData.origin, scope.draggableList);
+          if ((e.target || e.srcElement).parentElement === dragData.elem.parentElement) {
+            angular.copy(dragData.origin, scope.draggableList);
+          }
         });
 
         elem.bind('drop', function (e) {
-          e.preventDefault();
-
-          scope.$apply(function () {
-            scope.draggableList.splice(scope.$parent.$index, 0, scope.draggableList.splice(dragData.from_index, 1)[0]);
-          });
+          e.preventDefault(); // important! stop firefox redirecting
+          if ((e.target || e.srcElement).parentElement === dragData.elem.parentElement) {
+            scope.$apply(function () {
+              scope.draggableList.splice(scope.$parent.$index, 0, scope.draggableList.splice(dragData.from_index, 1)[0]);
+            });
+          }
         });
 
         elem.bind('dragend', function (e) {
