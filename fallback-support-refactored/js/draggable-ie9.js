bb79ee87a465686ca081e55a39dcfea43dfb2d93
nverba <distantstate@gmail.com>
2014-03-04 19:46:12 +0000

    partial save

---
+++ fallback-support-refactored/js/draggable-ie9.js
@@ -0,0 +1,16 @@
+// MDN CustomEvent constructor pollyfill
+
+(function () {
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   };
+
+  CustomEvent.prototype = window.CustomEvent.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
+var event = new CustomEvent("dragenter");
